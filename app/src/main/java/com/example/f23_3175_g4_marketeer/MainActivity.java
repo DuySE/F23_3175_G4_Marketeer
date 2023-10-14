676831deebdb6499c910a98f279727cf6c34e389
DuySE <duynse@gmail.com>
2023-10-13 22:43:56 -0700

    Initialize commit

---
+++ app/src/main/java/com/example/f23_3175_g4_marketeer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.f23_3175_g4_marketeer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
