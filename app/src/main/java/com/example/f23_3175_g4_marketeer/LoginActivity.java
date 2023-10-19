b6839781b5b1fa80a9bef2d48d8061ff93036e1f
Jack Braun <braunjack2003@gmail.com>
2023-10-19 14:57:36 -0700

    Feature: added simple registration page.

--- app/src/main/java/com/example/f23_3175_g4_marketeer/LoginActivity.java
+++ app/src/main/java/com/example/f23_3175_g4_marketeer/LoginActivity.java
@@ -10,6 +10,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
 
     //no create account feature yet
@@ -17,33 +20,56 @@ public class LoginActivity extends AppCompatActivity {
     TextView textViewLogin;
     EditText editTextUsername;
     EditText editTextPassword;
-    Button btnLogin;
-    Intent intent;
+    Button btnLogin, btnRegister;
+    Intent intentMain, intentRegister;
+    String username, password;
+
+    //couldnt figure out how to store new accounts in array, so i will come back to this when the database is implemented
+    //List<String> usernameList = new ArrayList<>();
+    //List<String> passwordList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        textViewLogin = findViewById(R.id.textViewLogin);
-        editTextUsername = findViewById(R.id.editTextUsername);
-        editTextPassword = findViewById(R.id.editTextPassword);
+        try {
+            Bundle bundle = getIntent().getExtras();
+            username = bundle.getString("USERNAME", "");
+            password = bundle.getString("PASSWORD", "");
+            //usernameList.add(username);
+            //passwordList.add(password);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        textViewLogin = findViewById(R.id.textViewLoginTitle);
+        editTextUsername = findViewById(R.id.editTextUsernameLogin);
+        editTextPassword = findViewById(R.id.editTextPasswordLogin);
         btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
         Login();
     }
 
     private void Login() {
-        intent = new Intent(this, MainActivity.class);
+        intentMain = new Intent(this, MainActivity.class);
         btnLogin.setOnClickListener((View view) -> {
             if(editTextUsername.getText().toString().isEmpty()) {
                 Toast.makeText(this, "Must input username", Toast.LENGTH_SHORT).show();
             } else if(editTextPassword.getText().toString().isEmpty()) {
                 Toast.makeText(this, "Must input password", Toast.LENGTH_SHORT).show();
-            } else {
-                if(editTextUsername.getText().toString().equals("admin") && editTextPassword.getText().toString().equals("admin")) {
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(this, "Username or password not found", Toast.LENGTH_SHORT).show();
-                }
+            } else if(editTextPassword.getText().toString().equals("admin") && editTextUsername.getText().toString().equals("admin")) {
+                startActivity(intentMain);
             }
+            else if(editTextUsername.getText().toString().equals(username) && editTextPassword.getText().toString().equals(password)) {
+                startActivity(intentMain);
+            }
+                /*for(int i = 0; i < usernameList.size(); i++) {
+                    if(editTextUsername.getText().toString().equals(usernameList.get(i)) && editTextPassword.getText().toString().equals(passwordList.get(i))) {
+                        startActivity(intentMain);
+                    }
+                }*/
+        });
+        btnRegister.setOnClickListener((View view) -> {
+            intentRegister = new Intent(this, RegisterActivity.class);
+            startActivity(intentRegister);
         });
     }
 }
