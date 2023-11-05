8af0da2fed4f7849f373c1050560aa9d5e1b7148
LukeyLuke237 <116841654+LukeyLuke237@users.noreply.github.com>
2023-11-04 18:41:06 -0700

    Improve security for profile by hiding password and making user enter current password in order to edit

--- app/src/main/java/com/example/f23_3175_g4_marketeer/MyProfileActivity.java
+++ app/src/main/java/com/example/f23_3175_g4_marketeer/MyProfileActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,11 +21,9 @@ import java.util.TimerTask;
 
 public class MyProfileActivity extends AppCompatActivity {
     TextView txtViewUsername;
-    TextView txtViewPassword;
     TextView txtViewPhone;
     TextView txtViewAddress;
-    Button btn;
-    Button btn2;
+    Button btnEditProfile;
     ImageView imgView;
     String imgName;
     User user;
@@ -35,15 +34,13 @@ public class MyProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
         txtViewUsername = findViewById(R.id.txtViewUsername);
-        txtViewPassword = findViewById(R.id.txtViewUserPassword);
         txtViewPhone = findViewById(R.id.txtViewPhone);
         txtViewAddress = findViewById(R.id.txtViewAddress);
-        btn = findViewById(R.id.btnEditProfile);
-        btn2 = findViewById(R.id.btnShowPassword);
+        btnEditProfile = findViewById(R.id.btnEditProfile);
         imgView = findViewById(R.id.imgViewPfp);
         setProfile();
       
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnEditProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Bundle bundle = new Bundle();
@@ -79,29 +76,6 @@ public class MyProfileActivity extends AppCompatActivity {
                 startActivity(intent);
             } 
         });
-        btnShowPassword.setOnClickListener(view -> showPassword());
-    }
-
-    private void showPassword() {
-        String password = "";
-        String username = StoredDataHelper.get(this, "username");
-        User user = databaseHelper.getUser(username);
-        try {
-            password = user.getPassword();
-            String passwordStr = "";
-            if (btnShowPassword.getText().toString().equals("SHOW")) {
-                txtViewPassword.setText(password);
-                btnShowPassword.setText(R.string.txtHide);
-            } else {
-                for (int i = 0; i < password.length(); i++) {
-                    passwordStr += "*";
-                    txtViewPassword.setText(passwordStr);
-                }
-                btnShowPassword.setText(R.string.txtShow);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public void setProfile() {
@@ -131,14 +105,6 @@ public class MyProfileActivity extends AppCompatActivity {
             };
 
             Timer timer = new Timer();
-            if (password == null) {
-                txtViewPassword.setText(R.string.error);
-            } else {
-                for (int i = 0; i < password.length(); i++) {
-                    hiddenPassword += "*";
-                }
-                txtViewPassword.setText(hiddenPassword);
-            }
             timer.schedule(timerTask,3000);
         } catch (Exception e) {
             e.printStackTrace();
