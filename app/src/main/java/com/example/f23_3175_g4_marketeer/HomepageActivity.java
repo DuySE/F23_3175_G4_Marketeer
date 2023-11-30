177700f10cdb8b429e5beb25cfe946ac3e5375bf
Jack-Braun <braunjack2003@gmail.com>
2023-11-29 17:57:38 -0800

    Small update to homepage, which shows current logged in username and small update to nav_header removing a textView

--- app/src/main/java/com/example/f23_3175_g4_marketeer/HomepageActivity.java
+++ app/src/main/java/com/example/f23_3175_g4_marketeer/HomepageActivity.java
@@ -2,13 +2,14 @@ package com.example.f23_3175_g4_marketeer;
 
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.f23_3175_g4_marketeer.databinding.ActivityHomepageBinding;
 
 //This activity is mainly here to add a buffer so user wont crash on back button press.
 public class HomepageActivity extends DrawerActivity {
     ActivityHomepageBinding homepageBinding;
-
+    User user;
     @Override
     protected void SetUpBackButton() {
         //Do nothing (to avoid a crash)
@@ -20,5 +21,12 @@ public class HomepageActivity extends DrawerActivity {
         homepageBinding = ActivityHomepageBinding.inflate(getLayoutInflater());
         setContentView(homepageBinding.getRoot());
         allocateActivityTitle("Homepage");
+
+        DatabaseHelper db = new DatabaseHelper(this);
+        user = db.getUser(StoredDataHelper.get(this, "username"));
+        TextView textViewUsername = findViewById(R.id.textHomeUsername);
+        textViewUsername.setText(user.getUsername());
+
+
     }
 }
